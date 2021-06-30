444fa0e74c08ebe62a2431340052b507bb42f55a
MobilizeSpa <43625714+MobilizeSpa@users.noreply.github.com>
2021-06-30 16:38:47 -0500

    Delete purchase_tracking directory

--- purchase_tracking/models/sale_order.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    tracking_purchase_order_id = fields.Many2one('purchase.order')
